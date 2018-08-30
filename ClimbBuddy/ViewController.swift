63e706c7a6638a329edd05a740a54300825629c7
Kristine Otto <kristineotto@kristines-macbook-pro.local>
2018-08-29 22:32:32 -0400

    Initial Commit

---
+++ ClimbBuddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClimbBuddy
+//
+//  Created by Kristine Otto on 8/29/18.
+//  Copyright © 2018 Kristine Otto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
