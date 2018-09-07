a3d5bfdf23f2e2aa9e00b8ef5ca20d7cb35846ba
Kristine Otto <kristineotto@Kristines-MacBook-Pro.local>
2018-09-06 21:26:36 -0400

    Cleaned Up Code

--- ClimbBuddy/Controllers/LoginViewController.swift
+++ ClimbBuddy/Controllers/LoginViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import FBSDKLoginKit
 import FBSDKCoreKit
 
-class LoginViewController: UIViewController /*,FBSDKLoginButtonDelegate*/ {
+class LoginViewController: UIViewController {
     
     @IBOutlet weak var FacebookLogInButton: FBSDKButton!
     
@@ -21,84 +21,18 @@ class LoginViewController: UIViewController /*,FBSDKLoginButtonDelegate*/ {
             FacebookLogInButton.setTitle("Log In", for: .normal)
         }else{
             print("Logged in already")
-            //FacebookLogInButton.titleLabel?.text = "Log Out"
             FacebookLogInButton.setTitle("Log Out", for: .normal)
         }
         
-        /*let loginButton = FBSDKLoginButton(type: .system)
-        loginButton.delegate = self
-        loginButton.readPermissions = ["public_profile","email"]
-        loginButton.center = self.view.center
-        self.view.addSubview(loginButton)*/
         super.viewDidLoad()
     }
     
-    
-    /*func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
-        print("logged in")
-        let canceledBool = result.isCancelled
-        
-        guard canceledBool != true else {
-            print("User cancelled Facebook log in")
-            return
-        }
-        
-        let permissionDenied = !result.declinedPermissions.isEmpty
-        guard permissionDenied != true else {
-            print("Permission was denied")
-            return
-        }
-        //let grantedPermissions = !result.grantedPermissions.isEmpty
-        //let token = result.token!
-        let request = FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields":"first_name,last_name,email, picture.type(large)"], httpMethod: "GET")!
-        request.start(completionHandler: { (requestResult, response, error) in
-            if error == nil {
-                if let responseDictionary = (response as? NSDictionary) {
-                    let email = responseDictionary["email"] as! String
-                    let firstName = responseDictionary["first_name"] as! String
-                    let lastName = responseDictionary["last_name"] as! String
-                    let pictDict =  responseDictionary["picture"] as? NSDictionary
-                    let pictureData = pictDict?["data"] as? NSDictionary
-                    let pictureUrl = pictureData?["url"] as! String
-                    
-                    UserDefaults.sharedInstance.loggedInUser = UserProfile(email: email, firstName: firstName, lastName: lastName, profilePictureURL: pictureUrl)
-                    
-                }
-            }
-            else{
-                print(error!.localizedDescription)
-            }
-        })
-        print("Access Granted")
-    }
-    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
-        print("logged out")
-        UserDefaults.sharedInstance.loggedInUser = nil
-    }*/
     @IBAction func LoginButton_Tap(_ sender: Any) {
         if FacebookLogInButton.titleLabel?.text == "Log In" {
-            let fbLoginManager : FBSDKLoginManager = FBSDKLoginManager()
-            fbLoginManager.logIn(withReadPermissions: ["email"], from: self) { (result, error) -> Void in
-                if (error == nil){
-                    let fbloginresult : FBSDKLoginManagerLoginResult = result!
-                    // if user cancel the login
-                    if (result?.isCancelled)!{
-                        return
-                    }
-                    if(fbloginresult.grantedPermissions.contains("email"))
-                    {
-                        self.getFBUserData()
-                        //self.reloadInputViews()
-                        self.viewDidLoad()
-                    }
-                }
-            }
+            logIn()
         }
         else {
-            UserDefaults.sharedInstance.loggedInUser = nil
-            FBSDKLoginManager().logOut()
-            //self.reloadInputViews()
-            self.viewDidLoad()
+            self.logOut()
         }
     }
     @IBAction func addClimbButton_Tap(_ sender: Any) {
@@ -121,6 +55,29 @@ class LoginViewController: UIViewController /*,FBSDKLoginButtonDelegate*/ {
                     }
                 }
             })
+        }
+    }
+    func logOut() {
+        UserDefaults.sharedInstance.loggedInUser = nil
+        FBSDKLoginManager().logOut()
+        self.viewDidLoad()
+    }
+    
+    func logIn() {
+        let fbLoginManager : FBSDKLoginManager = FBSDKLoginManager()
+        fbLoginManager.logIn(withReadPermissions: ["email"], from: self) { (result, error) -> Void in
+            if (error == nil){
+                let fbloginresult : FBSDKLoginManagerLoginResult = result!
+                // if user cancel the login
+                if (result?.isCancelled)!{
+                    return
+                }
+                if(fbloginresult.grantedPermissions.contains("email"))
+                {
+                    self.getFBUserData()
+                    self.viewDidLoad()
+                }
+            }
         }
     }
 }
